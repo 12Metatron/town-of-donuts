518d0bd4e8c77321b4e05f888d23453cd064e721
Luffy <zacharykaufman200@gmail.com>
2024-10-21 21:47:07 -0400

    improved navbar significantly

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,32 +7,34 @@ import "./navbar.css";
 function Navbar() {
     return (
         <nav className="navbar">
-            <Link to="/" className="navbar-title"> 
-                <img src={require("../assets/Town_of_Donuts.png")} alt="donut logo" width='50' height='50' id="navlogo"/>
-                <button className="navbutton">Town of Donuts</button>
-            </Link>
-            <div className="navbar-main">
-                <Link to="/roles" className="navbar-title"> 
-                    <button className="navbutton">
-                        <span className="habibi-regular"> Roles</span>
-                    </button>
+            <ul className="navlist">
+                <Link to="/" className="navbar-title"> 
+                    <img src={require("../assets/Town_of_Donuts.png")} alt="donut logo" width='35' height='35' id="navlogo"/>
+                    <li className="navbutton">Town of Donuts</li>
                 </Link>
-                {/* <Link to="/about" className="navbar-title"> 
-                    <button className="navbutton">
-                        <span className="habibi-regular">About</span>
-                    </button>
-                </Link> */}
-                <Link to="/rules" className="navbar-title"> 
-                    <button className="navbutton">
-                        <span className="habibi-regular">Rules</span>
-                    </button>
-                </Link>
-                <Link to="/achievements" className="navbar-title"> 
-                    <button className="navbutton">
-                        <span className="habibi-regular">Achievements</span>
-                    </button>
-                </Link>
-            </div> 
+                <div className="navbar-main">
+                    <Link to="/roles" className="navbar-title"> 
+                        <li className="navbutton">
+                            <span className="habibi-regular"> Roles</span>
+                        </li>
+                    </Link>
+                    {/* <Link to="/about" className="navbar-title"> 
+                        <button className="navbutton">
+                            <span className="habibi-regular">About</span>
+                        </button>
+                    </Link> */}
+                    <Link to="/rules" className="navbar-title"> 
+                        <li className="navbutton">
+                            <span className="habibi-regular">Rules</span>
+                        </li>
+                    </Link>
+                    <Link to="/achievements" className="navbar-title"> 
+                        <li className="navbutton">
+                            <span className="habibi-regular">Achievements</span>
+                        </li>
+                    </Link>
+                </div>
+            </ul> 
 
         </nav>
     );
