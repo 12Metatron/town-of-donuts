6f5e76efd22b790e680a8a553bcea38cf380525b
noClaps <04plugs-bios@icloud.com>
2024-11-02 21:21:56 +0000

    feat: Use ul for lists

--- src/components/rolecard.jsx
+++ src/components/rolecard.jsx
@@ -3,31 +3,58 @@ import "./rolecard.css";
 //import role_list from "../data/role_list";
 
 function RoleCard(props) {
-    const specialLines = props.special.split('\n');
-    const abilityLines = props.abilities.split('\n');
-    const informationLines = props.information.split('\n');
-    return (
-        <div className={`${props.alignment} role`}>
-            <dt>
-                <h3 className="londrina-sketch-regular rolename">{props.name}</h3>
-                <p className="alignment"><span className="underline">Alignment:</span> {props.alignment}</p>
-                <p className="immunities"><span className="underline">Immunities</span> - {props.immunities}</p>
-                <p className="attack"><span className="underline">Attack</span> - {props.attack}</p>
-                <p className="defence"><span className="underline">Defence</span> - {props.defence}</p>
-                <p className="time"><span className="underline">Time</span> - {props.time}</p>
-                <span className="special-body"><span className="underline">Special:</span> 
-                {specialLines.map((line, index) => (<p className="special">• {line}</p>))}
-                </span>
-                <p className="abilities-body"><span className="underline">Abilities:</span>
-                {abilityLines.map((line, index) => (<p className="abilities">• {line}</p>))}
-                 </p>
-                <p className="information-body"><span className="underline">Extra:</span> 
-                {informationLines.map((line, index) => (<p className="information">• {line}</p>))}
-                </p>
-                <p className="goal"><span className="underline">Goal:</span> {props.goal}</p>
-            </dt>
-        </div>
-    );
+  const specialLines = props.special.split("\n");
+  const abilityLines = props.abilities.split("\n");
+  const informationLines = props.information.split("\n");
+  return (
+    <div className={`${props.alignment} role`}>
+      <dt>
+        <h3 className="londrina-sketch-regular rolename">{props.name}</h3>
+        <p className="alignment">
+          <span className="underline">Alignment:</span> {props.alignment}
+        </p>
+        <p className="immunities">
+          <span className="underline">Immunities</span> - {props.immunities}
+        </p>
+        <p className="attack">
+          <span className="underline">Attack</span> - {props.attack}
+        </p>
+        <p className="defence">
+          <span className="underline">Defence</span> - {props.defence}
+        </p>
+        <p className="time">
+          <span className="underline">Time</span> - {props.time}
+        </p>
+        <span className="special-body">
+          <span className="underline">Special:</span>
+          <ul>
+            {specialLines.map((line) => (
+              <li className="special">{line}</li>
+            ))}
+          </ul>
+        </span>
+        <p className="abilities-body">
+          <span className="underline">Abilities:</span>
+          <ul>
+            {abilityLines.map((line) => (
+              <li className="abilities">{line}</li>
+            ))}
+          </ul>
+        </p>
+        <p className="information-body">
+          <span className="underline">Extra:</span>
+          <ul>
+            {informationLines.map((line) => (
+              <li className="information">{line}</li>
+            ))}
+          </ul>
+        </p>
+        <p className="goal">
+          <span className="underline">Goal:</span> {props.goal}
+        </p>
+      </dt>
+    </div>
+  );
 }
 
 export default RoleCard;
